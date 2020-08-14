f763e8a23eae7cd200adf704fb025a6e84a9f440
Pablo Gonzalez <pabgon0@gmail.com>
2020-08-14 21:26:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 let dodger = document.getElementById("dodger");
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
 
 function moveDodgerLeft() {
   let leftNumbers = dodger.style.left.replace("px", "");
